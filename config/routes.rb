bc75d36dcd5c87732eb8e29239d4cb8d618ce69a
rabea bader <bdr193@gmail.com>
2017-11-27 15:55:22 +0100

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+
+  get 'rooms/show'
+
+  get 'rooms/new'
+
+  get 'rooms/edit'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
