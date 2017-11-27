e9938c16a9df790d7e3ef28002e54896e16c4be9
rabea bader <bdr193@gmail.com>
2017-11-27 19:12:45 +0100

    Finished

--- config/routes.rb
+++ config/routes.rb
@@ -8,45 +8,8 @@ Rails.application.routes.draw do
 
   resources :requests, only: [:index, :show, :new, :edit]
 
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
-  # get 'viewings/index'
-
-  # get 'viewings/show'
-
-  # get 'viewings/new'
-
-  # get 'viewings/edit'
-
-  # get 'requests/index'
-
-  # get 'requests/show'
-
-  # get 'requests/new'
-
-  # get 'requests/edit'
-
-  # get 'flats/show'
-
-  # get 'flats/new'
-
-<<<<<<< HEAD
-  devise_for :users
-  
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-=======
-  # get 'flats/edit'
-
-  # get 'rooms/index'
-
-  # get 'rooms/show'
-
-  # get 'rooms/new'
-
-  # get 'rooms/edit'
->>>>>>> master
