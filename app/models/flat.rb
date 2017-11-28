66f2df3a3618d43554daa7412cc214c08edad777
Rich Ogrady <rjogrady21@gmail.com>
2017-11-28 13:00:23 +0100

    Started to update the map functionality

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,6 @@
 class Flat < ApplicationRecord
   has_many :users
   has_many :rooms
+  geocoded_by :address
+  after_validation :geocode
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127175109) do
+ActiveRecord::Schema.define(version: 20171128115228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20171127175109) do
     t.string   "neighborhood"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "requests", force: :cascade do |t|
