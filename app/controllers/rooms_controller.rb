13eaab34e689e9a1380f098023049a1709f22a76
Rich Ogrady <rjogrady21@gmail.com>
2017-11-29 19:00:14 +0100

    Functioning search from landing page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,12 +1,35 @@
 class RoomsController < ApplicationController
   before_action :find_room_id, only: [:show, :edit]
 
+  def info(opts = {})
+    opts.each do |key, value|
+      session[key.to_sym] = value
+    end
+  end
+
   def index
-    @city = params[:room][:city] || session[:city]
-    @move_in_date = params[:room][:move_in_date] || session[:move_in_date]
-    @move_out_date = params[:room][:move_out_date] || session[:move_out_date]
-    info(city: @city, move_in_date: @move_in_date, move_out_date: @move_out_date)
-    @rooms = Room.all
+
+
+    if params[:room]
+       # @flats = Flat.joins(:rooms).near([params[:room][:lat],params[:room][:lng]], 15)
+      if params[:room][:city]
+        @flats_ids = Flat.near([52.5075681, 13.3994533], 15).map(&:id)
+        @rooms = Room.where(flat_id: @flats_ids)
+      else
+        @rooms = Room.all
+      end
+      @rooms = @rooms.where("move_in_date < ?", params[:room][:move_in_date]) unless params[:room][:move_in_date].blank?
+      @rooms = @rooms.where("move_out_date > ?", params[:room][:move_out_date]) unless params[:room][:move_out_date].blank?
+      # raise # delete after AR
+      # @rooms = @flats.joins(:rooms)
+      # @rooms = @rooms.where(:room => { move_in_date: params[:room][:move_in_date]}) if params[:room][:move_in_date]
+      # @rooms = @rooms.where(:room => { move_out_date: params[:room][:move_out_date]}) if params[:room][:move_out_date]
+
+      # info( move_in_date: params[:room][:move_in_date], params[:room][:move_out_date])
+
+    else
+      @rooms = Room.all
+    end
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,58 +8,63 @@
 
 puts "Clearing database"
 
-puts "Clearing users"
-User.destroy_all
-puts "Clearing flats"
-Flat.destroy_all
-puts "Clearing rooms"
-Room.destroy_all
 puts "Clearing requests"
 Request.destroy_all
 puts "Clearing Viewings"
 Viewing.destroy_all
+puts "Clearing rooms"
+Room.destroy_all
+puts "Clearing flats"
+Flat.destroy_all
+puts "Clearing users"
+User.destroy_all
 
 puts "Creating users"
 
-user_one = User.create!(
+user_one = User.new(
   email: "rabea_bader@gmail.com",
   password: "secret123",
   first_name: "Rabea",
   last_name: "Badea",
   # facebook_picture_url: "https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/11267855_494475644050145_7650517800177078839_n.jpg?oh=88f598f1fc8228a3a81f204d69ab47bb&oe=5A916072"
 )
+user_one.save
 
-user_two = User.create!(
+user_two = User.new(
   email: "sarah_lafer@yahoo.com",
   password: "secret123",
   first_name: "Sarah",
   last_name: "Lafer",
   # facebook_picture_url: "https://scontent-frt3-1.xx.fbcdn.net/v/t31.0-8/15123387_1524489700911298_8540080221667972968_o.jpg?oh=81c74c24d54e1d3f4989184e7a9ee45a&oe=5AA0CACD"
 )
+user_two.save
 
-user_three = User.create!(
+user_three = User.new(
   email: "stephan_is_great@gmail.com",
   password: "secret123",
   first_name: "Stephan",
   last_name: "Schmidbauer",
   # facebook_picture_url: "https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/12472516_1150297835028069_8300065499297260894_n.jpg?oh=61fd7f780e66b7d600be2918c009e714&oe=5AA8DD43"
 )
+user_three.save
 
-user_four = User.create!(
+user_four = User.new(
   email: "richard_ogrady@gmail.com",
   password: "secret123",
   first_name: "Richard",
   last_name: "O'Grady",
   # facebook_picture_url: "https://scontent-frt3-1.xx.fbcdn.net/v/t31.0-8/15972751_237246466721822_1634594414421206870_o.jpg?oh=51c4df723a1a2e64c7972c3e34468b0b&oe=5A8F41B5"
 )
+user_four.save
 
-user_five = User.create!(
+user_five = User.new(
   email: "david_beckham@hotmail.com",
   password: "secret123",
   first_name: "David",
   last_name: "Beckham",
   # facebook_picture_url: "https://www.famousbirthdays.com/headshots/david-beckham-4.jpg"
 )
+user_five.save
 
 users = [user_one, user_two, user_three, user_four, user_five]
 
@@ -74,7 +79,7 @@ flat_one_urls = [
 'https://a0.muscache.com/im/pictures/19755768/95112653_original.jpg?aki_policy=x_large'
 ]
 
-flat_one = Flat.create!(
+flat_one = Flat.new(
   title: "Flatshare - We're lovin it!",
   description: "This large & modern penthouse is in a great, central area & consists of 3 floors, all boasting unbelievable views. The top floor has a private plunge pool & magical 360° vistas. Situated on the slopes of Table Mountain & close to everything!",
   monthly_price: 440,
@@ -82,33 +87,36 @@ flat_one = Flat.create!(
   amenities: "Internet, Free parking, Smoking allowed",
   address: "Axel-Springer-Straße 41, 10969 Berlin",
   neighborhood: "Kreuzberg",
-  photo_urls: flat_one_urls
+  photo_urls: []
   # latitude: 52.507535,
   # longitude: 13.399219,
   )
 
+flat_one.save
+
 puts "Flat 1 created"
 
-room_one = Room.create!(
-  move_in_date: '01-12-2017',
-  move_out_date: '01-05-2018',
-  flat_id: flat_one[:id],
+room_one = Room.new(
+  move_in_date: Date.parse('01-12-2017'),
+  move_out_date: Date.parse('01-05-2018'),
+  flat: flat_one,
   )
-
+room_one.save
 puts "Room 1 created"
 
 
-viewing_one = Viewing.create!(
+viewing_one = Viewing.new(
   start_time: Time.strptime('28/11/2017 19:00', '%d/%m/%Y %H:%M'),
   duration: 45,
-  room_id: 1,
+  room: room_one,
   )
+viewing_one.save
 
 puts "Viewing 1 created"
-request_one = Request.create!(
+request_one = Request.new(
   slot: Time.strptime('28/11/2017 19:00', '%d/%m/%Y %H:%M'),
-  user_id: 1,
-  viewing_id: 1,
+  user_id: user_one,
+  viewing: viewing_one,
   )
 request_one.accepted!
 request_one.save!
