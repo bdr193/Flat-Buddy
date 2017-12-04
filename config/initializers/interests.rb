d99cd92a4e85a7d641f82fe65d25fa646c3ef04c
Stephan Schmidbauer <schmidbauer.stephan@gmail.com>
2017-12-04 17:28:40 +0100

    created initializer interests

---
+++ config/initializers/interests.rb
@@ -0,0 +1,8 @@
+INTERESTS = {
+  arts: ["Museums, Exhibitions & Galleries", "Photography & Film Creation", "Theatre, Musicals & Opera", "Live music or comedy", "Cinema", "Reading and Literature"],
+  sports: ["Ball sports", "Endurance sports", "Wellbeing sports", "Snow sports", "Extreme sports", "Watersports"],
+  films: ["Romance & Comedy", "Action", "Horror", "Documentary", "Animation & Cartoon", "Science Fiction"],
+  music: ["Pop", "Electronic", "Classical & Opera", "Rock & Heavy Metal", "Jazz", "Hip Hop & Rap"],
+  holiday: ["Relaxing beach holiday", "Trekking adventure", "City-break (Shopping, Food and Chill)", "Party holiday", "Stay-at-home holiday", "Historical city exploration"],
+  food: ["A vegy salad", "Meat is all I need", "Quick pasta meal", "Let’s go out for dinner", "Delivery to my bed", "Whatever the food is, the drink is more important"]
+}
