98a29030cf6937dbd7d219ecd20290b055a11f39
rabea bader <bdr193@gmail.com>
2017-12-04 12:19:40 +0100

    uncomented the search validation from rooms controlelr

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -10,11 +10,11 @@ class RoomsController < ApplicationController
   def index
     @neighborhoods = ["Baker Street", "Battersea", "Bayswater", "Bermondsey", "Bethnal Green", "Brick Lane", "Brixton", "Brixton Hill", "Camberwell", "Chalk Farm", "Chelsea", "Clapham", "Clapham North", "Dalston", "Earls Court", "Elephant & Castle", "Finsbury", "Fitzrovia", "Fulham", "Hoxton", "Islington", "Kensington", "Kentish Town", "King's Cross", "Lower Holloway", "Maida Hill", "Marylebone", "Notting Hill", "Old Street", "Paddington", "Shadwell", "Shoreditch", "Southwark", "Spitalfields", "Stockwell", "Surrey Quays", "Swiss Cottage", "Vauxhall", "Wapping", "Waterloo", "West Brompton", "Whitechapel"]
 
-    @lat = params[:search][:lat] || session[:lat]
-    @lng = params[:search][:lng] || session[:lng]
+    @lat = session[:lat] || params[:search][:lat]
+    @lng = session[:lng] || params[:search][:lng]
 
-    @move_in_date = params[:search][:move_in_date] || session[:move_in_date]
-    @move_out_date = params[:search][:move_out_date] || session[:move_out_date]
+    @move_in_date = session[:move_in_date] || params[:search][:move_in_date]
+    @move_out_date = session[:move_out_date] || params[:search][:move_out_date]
 
     @flats_ids = Flat.near([@lat, @lng], 30).map(&:id)
     @rooms = Room.where(flat_id: @flats_ids)
@@ -22,29 +22,31 @@ class RoomsController < ApplicationController
     @rooms = @rooms.where("move_in_date <= ?", @move_in_date) unless @move_in_date.blank?
     @rooms = @rooms.where("move_out_date >= ?", @move_out_date) unless @move_out_date.blank?
 
-    if params[:search][:neighborhood].present?
-      @neighborhood = params[:search][:neighborhood]
-      @rooms = @rooms.select("*").joins(:flat).where(flats: { neighborhood: @neighborhood } )
+    unless params[:room].nil?
+      if params[:room][:neighborhood].present?
+        @neighborhood = params[:room][:neighborhood]
+        @rooms = @rooms.select("*").joins(:flat).where(flats: { neighborhood: @neighborhood } )
+      end
+
+      if params[:room][:number_of_flatmates].present?
+        @number_of_flatmates = params[:room][:number_of_flatmates]
+        @rooms = @rooms.select("*").joins(:flat).where("flats.number_of_flatmates <= ?", @number_of_flatmates )
+      end
+
+      if params[:room][:monthly_price].present?
+        @monthly_price = params[:room][:monthly_price]
+        @rooms = @rooms.select("*").joins(:flat).where("flats.monthly_price <= ?", @monthly_price )
+      end
+
+      if params[:room][:allow_students].present?
+        @allow_students = params[:room][:allow_students]
+        @rooms = @rooms.select("*").joins(:flat).where("flats.allow_students = ?", @allow_students )
+      end
     end
 
-    if params[:search][:number_of_flatmates].present?
-      @number_of_flatmates = params[:search][:number_of_flatmates]
-      @rooms = @rooms.select("*").joins(:flat).where("flats.number_of_flatmates <= ?", @number_of_flatmates )
-    end
-
-    if params[:search][:monthly_price].present?
-      @monthly_price = params[:search][:monthly_price]
-      @rooms = @rooms.select("*").joins(:flat).where("flats.monthly_price <= ?", @monthly_price )
-    end
-
-    if params[:search][:allow_students].present?
-      @allow_students = params[:search][:allow_students]
-      @rooms = @rooms.select("*").joins(:flat).where("flats.allow_students = ?", @allow_students )
-    end
 
     info(move_in_date: @move_in_date, move_out_date: @move_out_date, lat: @lat, lng: @lng)
 
-    # allow_students
     # allow_pets
     # bills_included
     # allow_smokers
@@ -52,12 +54,13 @@ class RoomsController < ApplicationController
     # couples_allowed
     # ensuite
     # accessible
-    @search = Search.new(search_params)
-    if @search.valid?
-      @rooms = Room.all
-    else
-      render "pages/home"
-    end
+
+    # @search = Search.new(search_params)
+    # if @search.valid?
+      # @rooms = Room.all
+    # else
+    #   render "pages/home"
+    # end
   end
 
   def show
@@ -86,6 +89,8 @@ class RoomsController < ApplicationController
   end
 
   def search_params
-    params.require(:search).permit(:city, :move_in_date, :move_out_date, :lat, :lng)
+    search = params.require(:search).permit(:city, :move_in_date, :move_out_date, :lat, :lng) unless params[:search].nil?
+    room = params.require(:room).permit(:neighborhood, :number_of_flatmates, :furnished, :monthly_price, :bills_included, :ensuite, :accessible, :allow_students, :allow_pets, :couples_allowed, :allow_smokers) unless params[:room].nil?
+    return search || room
   end
 end
