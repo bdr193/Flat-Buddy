69ac8d8f782948a5d5421eab2765be291fa640bd
rabea bader <bdr193@gmail.com>
2017-12-06 18:19:08 +0100

    cancelled JS and added options in the form

--- app/assets/javascripts/date_picker.js
+++ app/assets/javascripts/date_picker.js
@@ -1,6 +1,10 @@
-$('.datepicker').datepicker({
-  format: 'yyyy-mm-dd',
-  autoclose: true,
-  todayHighlight: true,
-  startDate: new Date(),
-});
+
+// var datepicker = $.fn.datepicker.noConflict(); // return $.fn.datepicker to previously assigned value
+// $.fn.bootstrapDP = datepicker;
+// $('.datepicker-bootstrap').datepicker({
+//   format: 'yyyy-mm-dd',
+//   autoclose: true,
+//   todayHighlight: true,
+//   startDate: new Date(),
+// });
+// console.log($('.datepicker-bootstrap'))
