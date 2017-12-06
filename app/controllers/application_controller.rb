510d4b7b48a8b1aea29c72731d0f34f0ba65a925
rabea bader <bdr193@gmail.com>
2017-12-06 16:11:16 +0100

    adding metadata

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     root_path
   end
+
+  def default_url_options
+    { host: ENV["flatbuddy.club"] || "localhost:3000" }
+  end
+
 end
