ade3629e201f8c1a6d36be3ac4799d42538d5b8d
rabea bader <bdr193@gmail.com>
2017-12-06 16:30:36 +0100

    fixed image size

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  def after_sign_in_path_for(resource)
-    root_path
-  end
-
   def default_url_options
     { host: ENV["flatbuddy.club"] || "localhost:3000" }
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path
   end
 
 end
