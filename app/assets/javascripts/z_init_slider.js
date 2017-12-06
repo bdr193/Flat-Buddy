fd5074633937e0553d2ed45946bd1e013a193b70
rabea bader <bdr193@gmail.com>
2017-12-06 11:06:25 +0100

    minor changes

--- app/assets/javascripts/z_init_slider.js
+++ app/assets/javascripts/z_init_slider.js
@@ -3,6 +3,13 @@ var mySlider = new Slider("#room_monthly_price", {
 	min: 100,
   max: 2000,
   step: 50,
-  value: 1200,
+	value: 1200,
   tooltip: 'always',
 });
+
+var value = mySlider.getValue();
+console.log("the current value is");
+console.log(value);
+
+console.log("setting the value");
+mySlider.setValue(value)
