401834f6442cdccf3743fe1ea9300b14e9d8454e
rabea bader <bdr193@gmail.com>
2017-12-07 12:37:44 +0100

    Calendar and slider works

--- app/assets/javascripts/z_init_slider.js
+++ app/assets/javascripts/z_init_slider.js
@@ -1,12 +1,16 @@
 // Instantiate a slider
-var mySlider = new Slider("#room_monthly_price", {
-	min: 100,
-  max: 2000,
-  step: 50,
-	value: 1200,
-  tooltip: 'always',
+$(document).ready(function(){
+	var regex = /monthly_price%5D=(\d+)&/
+	var loc = window.location.search
+	var val = parseInt(loc.match(regex)[1])
+
+	if ($("#room_monthly_price").length != 0) {
+		var mySlider = new Slider("#room_monthly_price", {
+			min: 100,
+		  max: 2000,
+		  step: 50,
+			value:  val || 1200,
+		  tooltip: 'always',
+		});
+	}
 });
-
-var value = mySlider.getValue();
-
-mySlider.setValue(value)
